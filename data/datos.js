a0437cdb4b9d31800a5314064b3ce5a128681a8a
thunder609 <oswaldo-pachco@hotmail.es>
2022-06-09 20:57:09 -0400

    pruebas

--- data/datos.js
+++ data/datos.js
@@ -13,51 +13,56 @@ export var perso =[
 {
                id:2,
                name:"Pekka",
-               descrip:'Es una tropa poderosa y puede hacer bastante daño con la colocación y el apoyo correctos. Hace daño masivo a un objetivo a la vez. Sólo a niveles decentes, una P.E.K.K.A puede matar a un Bárbaro de un solo golpe.',
+               descrip:'Pekka: Es una tropa poderosa y puede hacer bastante daño con la colocación y el apoyo correctos. Hace daño masivo a un objetivo a la vez. Sólo a niveles decentes, una P.E.K.K.A puede matar a un Bárbaro de un solo golpe.',
                img:'./Imagenes/Pekka.webp',
             },
 {
               id:3,
                name:"Caballero",
-               descrip:'Se puede utilizar como tanque para tropas más pequeñas como los Duendes. Es una gran carta para usar al frente de un mini-ataque. Tiene una gran sinergia en particular con los mazos de carnada de hechizos, y con el Barril de duendes, además es también común en mazos de Bait junto a la Valquiria.',
+               descrip:'Caballero: Se puede utilizar como tanque para tropas más pequeñas como los Duendes. Es una gran carta para usar al frente de un mini-ataque. Tiene una gran sinergia en particular con los mazos de carnada de hechizos, y con el Barril de duendes, además es también común en mazos de Bait junto a la Valquiria.',
                img:'./Imagenes/caballero.png',
 },
 {
 id :4,
 name:"Bruja nocturna",
-descrip:"es una tropa de base constructora que proviene de Clash Royale, junto con el bombardero y el bebé dragón. Sin embargo, dispara el mismo proyectil que la Bruja, a diferencia de su contraparte cuerpo a cuerpo de Clash Royale, a pesar de blandir un hacha.",
+descrip:"Bruja nocturna: es una tropa de base constructora que proviene de Clash Royale, junto con el bombardero y el bebé dragón. Sin embargo, dispara el mismo proyectil que la Bruja, a diferencia de su contraparte cuerpo a cuerpo de Clash Royale, a pesar de blandir un hacha.",
 img:'./Imagenes/bruja.png',
 },
 {
     id :5,
     name:"Los pillos",
-    descrip:"Está formada por dos &#34;niñas&#34; pillas y &#34;un niño pillo&#34;, el cual tiene la función de defender a sus amigas mientras estas lanzan &#34;chicles&#34; a sus enemigos. Por lo tanto, la función de esta carta es muy sencilla: realizar el daño con las niñas pillas mientras distraemos con el niño pillo.",
+    descrip:"Pillos:Está formada por dos &#34;niñas&#34; pillas y &#34;un niño pillo&#34;, el cual tiene la función de defender a sus amigas mientras estas lanzan &#34;chicles&#34; a sus enemigos. Por lo tanto, la función de esta carta es muy sencilla: realizar el daño con las niñas pillas mientras distraemos con el niño pillo.",
     img:'./Imagenes/los-pillos.png',
     },
     {
     id :6,
-    name:"Aquero",
-    descrip:"Dispara una flecha mágica que atraviesa y daña a los enemigos que se interponen en su camino. No es ningún truco, ¡es magia! Arquero mágico es una carta de rareza legendaria de Clash Royale que puede obtenerse a partir de la arena 5.",
+    name:"El Aquero",
+    descrip:"El Arquero:Dispara una flecha mágica que atraviesa y daña a los enemigos que se interponen en su camino. No es ningún truco, ¡es magia! Arquero mágico es una carta de rareza legendaria de Clash Royale que puede obtenerse a partir de la arena 5.",
     img:'./Imagenes/arquero.png',
     },
     {
-        id :6,
+        id :7,
         name:"Montapuercos:",
         descrip:"es una tropa terrestre rápida con puntos de vida medios, poco daño y la capacidad de saltar sobre los muros enemigos. Se desbloquea en el nivel 2 del Cuartel oscuro.",
         img:'./Imagenes/montaPuercos.webp',
         },
         {
-            id :7,
+            id :8,
             name:"Minero",
-            descrip:"Es una carta legendaria que puede obtenerse de los cofres una vez alcanzamos la arena 6. Cuesta 3 de elixir, una cifra bastante asequible, y puede desplegarse en cualquier parte de la Arena. Tiene bastantes puntos de vida, 1.000 a nivel 1 y de 1.460 a nivel 5, que no está nada mal",
+            descrip:"El minero:Es una carta legendaria que puede obtenerse de los cofres una vez alcanzamos la arena 6. Cuesta 3 de elixir, una cifra bastante asequible, y puede desplegarse en cualquier parte de la Arena. Tiene bastantes puntos de vida, 1.000 a nivel 1 y de 1.460 a nivel 5, que no está nada mal",
             img:'./Imagenes/Miner_info.webp',
             },
             {
-                id :8,
+                id :9,
                 name:"Principe",
-                descrip:"Es una tropa muy fuerte, además que tiene una gran velocidad, acompañar esta carta con otras unidades que ataquen con daño en área suele ser un buen combo.",
+                descrip:"El Principe:Es una tropa muy fuerte, además que tiene una gran velocidad, acompañar esta carta con otras unidades que ataquen con daño en área suele ser un buen combo.",
                 img:'./Imagenes/principe.png',
                 },
-
+                {
+                id :10,
+                name:"Principe",
+                descrip:"Sneaky goblings: es una Súper Tropa basada en el Goblin. Se puede desbloquear aumentando el Goblin cuando el Goblin tiene al menos el nivel 7 y cuando el Ayuntamiento se actualiza al nivel 11 o superior.",
+                img:'./Imagenes/Principe.png',
+                },
 
 ]
